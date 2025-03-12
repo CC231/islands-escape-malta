a822f44014edb23f40d5e5732613195fd9a492ca
CC231 <65074995+CC231@users.noreply.github.com>
2025-03-12 17:09:49 +0100

    Update ListingPage.js

--- src/containers/ListingPage/ListingPage.js
+++ src/containers/ListingPage/ListingPage.js
@@ -4,12 +4,14 @@ import ListingPricing from '../../components/ListingPricing';
 
 const ListingPricing = ({ listing }) => {
   const [isFullDay, setIsFullDay] = useState(true);
-
+  
   const fullDayPrice = listing.attributes.publicData.full_day_price || 0;
   const halfDayPrice = listing.attributes.publicData.half_day_price || 0;
 
   return (
+    <ListingPricing listing={listing} />
     <div className="pricing-section">
+    
       <label className="toggle-label">Half-Day</label>
       <label className="switch">
         <input
